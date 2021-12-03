0dc32abc7f0ab13839d2c99a15347c0c0cab4a10
Samyog Dhital <dhitalsamyog123@gmail.com>
2021-12-03 14:30:50 +0545

    nav bar almost complete

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
-import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <header>
+        <div className={styles.topnav}>
+          <a className={styles.active} href="#home">
+            Home
+          </a>
+          <div className={styles.nav_right}>
+            <a href="#news">About</a>
+            <a href="#contact">Projects</a>
+            <a href="#about">Skills</a>
+            <a href="#about">Tools</a>
+            <a href="#about">Contact</a>
+            <div className="icon"></div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
 }
