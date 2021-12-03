9ba7839098da4e247b814dff347a8148e1ba46c8
Samyog Dhital <dhitalsamyog123@gmail.com>
2021-12-03 22:05:55 +0545

    transparent nav with background image done

--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,27 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <header>
-        <div className={styles.topnav}>
-          <a className={styles.active} href="#home">
-            Home
-          </a>
-          <div className={styles.nav_right}>
-            <a href="#news">About</a>
-            <a href="#contact">Projects</a>
-            <a href="#about">Skills</a>
-            <a href="#about">Tools</a>
-            <a href="#about">Contact</a>
-            <div className="icon"></div>
+        <div className={styles.container}>
+          <div className={styles.topnav}>
+            <a className={styles.active} href="#home">
+              Home
+            </a>
+            <div className={styles.nav_right}>
+              <a href="#news">About</a>
+              <a href="#contact">Projects</a>
+              <a href="#about">Skills</a>
+              <a href="#about">Tools</a>
+              <a href="#about">Contact</a>
+              <div className="icon"></div>
+            </div>
           </div>
         </div>
       </header>
-    </div>
+      <main>
+        <div className={styles.img}></div>
+      </main>
+    </>
   );
 }
