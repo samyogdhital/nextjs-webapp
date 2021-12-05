eb194648cdef36b2355ef897dd23c95d11cc123f
Samyog Dhital <dhitalsamyog123@gmail.com>
2021-12-05 22:39:06 +0545

    some code changes

--- pages/index.js
+++ pages/index.js
@@ -18,11 +18,19 @@ export default function Home() {
               <div className="icon"></div>
             </div>
             <h1 className={styles.intro}>Samyog Dhital</h1>
+            <h3 className={styles.subintro}>Coder</h3>
           </div>
         </div>
       </header>
       <main>
         <div className={styles.img}></div>
+        <section className={styles.me}>
+          <img src="../images/IMG_5026.jpg" alt="" srcset="" />
+          <h2>About me</h2>
+          <div>
+            Hello this is me Samyog Dhital. I am a self taught programmer.
+          </div>
+        </section>
       </main>
     </>
   );
